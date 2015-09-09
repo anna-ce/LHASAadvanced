97b5acb77cbd5179f99f956758ab1373debb565f
cappelaere <pat@cappelaere.com>
2015-09-09 16:39:45 -0400

    replaced with generic index.ejs for esri

--- app/controllers/esri.js
+++ app/controllers/esri.js
@@ -2,9 +2,32 @@ module.exports = {
   
   	// List all
 	index: function(req, res) {	
-		var id = req.params['id']	
-		res.render("esri/"+id+".ejs", {
-			layout: false
+		var product	= req.params['id']
+		var host 		= "http://"+req.headers.host
+		var latitude	= 33.87
+		var longitude	= -90.77
+		
+		if( product === "precip_1d") {
+			source 	= "gpm"
+		} else if( product === 'landslide_nowcast') {
+			source  	= "landslide_model"
+			latitude	= 27
+			longitude	= 83
+		} else if( product === 'flood_nowcast') {
+			source 		= "gfms"
+			latitude	= 27
+			longitude	= 83
+		} else {
+			return res.send(404, "invalid product")
+		}
+		
+		res.render("esri/index.ejs", {
+			layout: 	false,
+			host: 		host,
+			product: 	product,
+			source: 	source,
+			latitude: 	latitude,
+			longitude: 	longitude
 		})			
 	}
 }
